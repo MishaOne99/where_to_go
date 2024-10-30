8a12e8e1dc9353a5b56860a3a8e71d6308d1d140
Misha <m.mazaixin@yandex.ru>
2024-10-30 18:05:16 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -7,9 +7,22 @@ class Places(models.Model):
     lon = models.FloatField(null=True, verbose_name='Долгота')
     lat = models.FloatField(null=True, verbose_name='Широта')
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
     
     class Meta:
         verbose_name = 'Место'
         verbose_name_plural = 'Места'
+
+
+class Images(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    number = models.IntegerField(blank=True, verbose_name='Номер изображения')
+    image = models.ImageField(blank=True)
+    
+    def __str__(self) -> str:
+        return f'№{self.number} - {self.title}'
+    
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'

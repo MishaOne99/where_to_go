37832b810c415505318777fa056fbfda0f605f5c
Misha <m.mazaixin@yandex.ru>
2024-11-01 15:56:00 +0300

    Упростил добавление фотографий в БД

--- places/admin.py
+++ places/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import Place, Image
 
 
-admin.site.register(Place)
+class PlaceElementInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceElementInline]
+
 admin.site.register(Image)

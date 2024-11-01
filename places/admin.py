6aaa8d89dc70fa1320690fa366d6f5d74f53c55b
Misha <m.mazaixin@yandex.ru>
2024-11-01 16:22:13 +0300

    Добавил отображения фото в БД

--- places/admin.py
+++ places/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Place, Image
 
 
 class PlaceElementInline(admin.TabularInline):
     model = Image
+    
+    fields = ('image', 'preview', 'image_id')
+    readonly_fields = ['preview']
+    
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 150px;">')
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [PlaceElementInline]
 
-admin.site.register(Image)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass

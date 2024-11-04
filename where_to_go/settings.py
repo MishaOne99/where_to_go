d37380c2434db4b51961fcc2987c9e0f326f9988
Misha <m.mazaixin@yandex.ru>
2024-11-04 14:38:29 +0300

    Уменьшил количество настроек, для первого запуска

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG',  default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='127.0.0.1')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='127.0.0.1,.pythonanywhere.com')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / env.str('DB_NAME'),
+        'NAME': BASE_DIR / env.str('DB_NAME', default='db.sqlite3'),
     }
 }
 

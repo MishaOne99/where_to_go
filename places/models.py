30c565aa9e993c95144376a794512a2c76e5f988
Misha <m.mazaixin@yandex.ru>
2024-11-04 15:02:38 +0300

    Упорядочил атрибуты модели данных

--- places/models.py
+++ places/models.py
@@ -11,18 +11,27 @@ class Place(models.Model):
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     
-    def __str__(self) -> str:
-        return self.title
-    
     class Meta:
         verbose_name = 'Место'
         verbose_name_plural = 'Места'
+    
+    def __str__(self) -> str:
+        return self.title
 
 
 class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Название локации', related_name='images')
     image = models.ImageField(verbose_name='Изображение')
     image_id = models.PositiveIntegerField(db_index=True, default=1, verbose_name='Номер изображения')
+    
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
+        
+        ordering = ['image_id']
+    
+    def __str__(self) -> str:
+        return str(self.place)
     
     def image_tag(self):
         if self.image:
@@ -31,11 +40,3 @@ class Image(models.Model):
 
     image_tag.short_description = 'Предпросмотр изображения'
     
-    def __str__(self) -> str:
-        return str(self.place)
-    
-    class Meta:
-        verbose_name = 'Изображение'
-        verbose_name_plural = 'Изображения'
-        
-        ordering = ['image_id']
